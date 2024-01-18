7b996eb3c86ace43176717896aa24bcadfbb4db7
okharyton <olegkhacv@gmail.com>
2024-01-18 13:15:37 +0200

    14Practice 12.12 (done). Add loading data in movie details side.

--- src/App.jsx
+++ src/App.jsx
@@ -137,6 +137,7 @@ export default function App() {
         <Box>
           {selectedId ? (
             <MovieDetails
+              KEY={KEY}
               selectedId={selectedId}
               onCloseMovie={handleCloseMovie}
             />
